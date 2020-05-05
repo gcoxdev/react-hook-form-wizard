996acd72c0246b88f5ae9f14a66d56eb1420aaed
Gary Cox <gary@gcoxdev.com>
2020-05-04 23:41:06 -0500

    fixed useWizard

--- hooks/useWizard.js
+++ hooks/useWizard.js
@@ -2,11 +2,11 @@ import { useState } from 'react'
 
 export const useWizard = ({ initialPage = 0 }) => {
     const [activePage, setActivePage] = useState(initialPage)
-    const [pageTotal, setPageTotal] = useState(0)
+    const [pageTotal, setPageTotal] = useState(1)
     const previousPage = () => setActivePage(activePage - 1)
     const nextPage = () => setActivePage(activePage + 1)
     const goToPage = (pageIndex) => setActivePage(pageIndex)
     const isLastPage = activePage === pageTotal - 1
 
-    return { activePage, pageTotal, previousPage, nextPage, goToPage, isLastPage }
+    return { activePage, pageTotal, previousPage, nextPage, goToPage, isLastPage, setPageTotal }
 }
